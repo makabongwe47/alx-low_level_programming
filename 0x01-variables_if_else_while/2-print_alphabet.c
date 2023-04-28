ecb10c5651590571fb8f07b0b5a0b08f9d1b8330
makabongwe47 <makabongwe47@gmail.com>
2023-04-28 14:43:27 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,15 +5,17 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int main()
 {
-	int c[26] = "abcdefghijklmnopqrstuvwxyz";
-	
 	int i;
-
-		for (i = 0; i < 26; i++)
+		
+	//ASCII value of a = 97 and z = 122
+	//ASCII characters from 97 to 122 are lowercase alphabets
+		for (i = 97; i <= 122; i++)
 		{
-			putchar(c[i]);
+			//The integer i with %c will be converted to a character
+			//Before printing .%c will take its equivalent character value
+			putchar("%c ", i);
 		}
 		putchar('\n');
 		return (0);
