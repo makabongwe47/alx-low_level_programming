03314c5d713f47ca679ef662860ed7c927464f30
makabongwe47 <makabongwe47@gmail.com>
2023-04-29 11:37:09 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/*
+ * main - Prints the alphabets in lowercase and uppercase.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+/*
+ * ASCII value of a = 97
+ * ASCII value of z = 122
+ */
+	for (i = 97; i <= 122; i++)
+	{
+/*
+ * The ASCII can be taken from i and display its character
+ * equivalent
+ */
+		putchar(i);
+	}
+/*
+ * ASCII value of A = 65
+ * ASCII value of Z = 90
+ */
+	for (i = 65; i <= 90; i++)
+	{
+/*
+ * The ASCII can be taken from i and display its character
+ * eqivalent
+ */
+		putchar(i);
+	}
+	putchar('\n')
+	return (0);
+}
+
+
