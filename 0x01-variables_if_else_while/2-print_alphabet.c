2beade422e82c210737dca8ba97e6dd750569dce
makabongwe47 <makabongwe47@gmail.com>
2023-04-29 09:41:10 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,16 +8,16 @@
 int main(void)
 {
 	int i;	
-	/* 
-	 * ASCII value of a = 97
-	 * ASCII value for z = 122
-	 */
+/*
+ * ASCII value of a = 97
+ * ASCII value for z = 122
+ */
 		for (i = 97; i <= 122; i++)
 		{
-		/* 
-		 * The ASCII should be taken from i and display
-		 * its character equivalent
-		 */
+/* 
+ * The ASCII should be taken from i and display
+ * its character equivalent
+ */
 			putchar(i);
 		}
 		putchar('\n');
