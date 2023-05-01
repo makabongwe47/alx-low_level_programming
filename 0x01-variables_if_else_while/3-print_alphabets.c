0b00ca68d20bb9eecfac735c2f360f21f84d685e
makabongwe47 <makabongwe47@gmail.com>
2023-05-01 03:40:38 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,38 +1,38 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * void: Null value
- *
- * Decription: Prints the alphabets in lowercase and uppercase
- * Return: Always 0 (Success)
- */
+  *main - Entry point
+  *void: Null value
+  *
+  *Decription: Prints the alphabets in lowercase and uppercase
+  *Return: Always 0 (Success)
+  */
 int main(void)
 {
-	int i, j;
-	i = 97;
-	j = 65;
+	int i;
+	int j;
+
 /**
- * ASCII value of a = 97
- * ASCII value of z = 122
- */
+  *ASCII value of a = 97
+  *ASCII value of z = 122
+  */
 	for (i = 97; i <= 122; i++)
 	{
 /**
- * The ASCII can be taken from i and display its character
- * equivalent
- */
+  *The ASCII can be taken from i and display its character
+  *equivalent
+  */
 		putchar(i);
 	}
 /**
- * ASCII value of A = 65
- * ASCII value of Z = 90
- */
+  *ASCII value of A = 65
+  *ASCII value of Z = 90
+  */
 	for (j = 65; j <= 90; j++)
 	{
 /**
- * The ASCII can be taken from i and display its character
- * eqivalent
- */
+  *The ASCII can be taken from j and display its character
+  *eqivalent
+  */
 		putchar(j);
 	}
 	putchar('\n');
