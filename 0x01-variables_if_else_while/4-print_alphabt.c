4bcd82f661a01dc7eb32fce93b21f5b827696d32
makabongwe47 <makabongwe47@gmail.com>
2023-05-01 07:40:34 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
 /**
  * main - Entry point
  *
  * Description: Prints lowercase alphabets and excludes q and e.
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int i = 97;
 
 	while (i <= 122)
 	{
-		if (i != 101 && i != 113)
+		if (i != 101 || i != 113)
 		{
 			i++;
 			continue;
