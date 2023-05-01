f490bfa7db6ec4bf3ec959858f47dcf52a9c4b0f
makabongwe47 <makabongwe47@gmail.com>
2023-05-01 10:28:02 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Prints all possible different combinations
+ * of three digits
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a
+	int b
+	int c
+
+	for (a = 48; a < 58; a++)
+	{
+		for (b = 49; b < 58; b++)
+		{
+			for (c = 50; c < 58; C++)
+			{
+				if (c > b && b > a)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a != 55 || b != 56)
+					{
+						putchar(',');
+						putcahar( );
+					}
+				}
+			}
+		}
+	}
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,1 @@
-A program that prints all possible different combinations of two digits
+A program that prints all possible different combinations of three digits
