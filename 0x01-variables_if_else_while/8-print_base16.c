74ab26d3a7654ee991182b40e7ca9ad3c5021838
makabongwe47 <makabongwe47@gmail.com>
2023-05-01 08:48:31 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Prints all numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	
+	for (j = 97; j <=102; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,1 @@
-A program that prints the lowercase alphabet in reverse
+A program that prints all the numbers of base 16 in lowercase, using only putchar()
